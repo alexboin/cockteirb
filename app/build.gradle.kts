68191ed98074d04f507b139e22059322b0ee9db5
Alexandre Boin <contact@alexandre-boin.fr>
2023-11-14 14:52:32 +0100

    :sparkles: Added basic tab layout and default fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "fr.aboin.cockteirb"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "fr.aboin.cockteirb"
--- app/src/main/java/fr/aboin/cockteirb/MainActivity.kt
+++ app/src/main/java/fr/aboin/cockteirb/MainActivity.kt
@@ -2,10 +2,63 @@ package fr.aboin.cockteirb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.tabs.TabLayout
+import fr.aboin.cockteirb.ui.categories.CategoriesFragment
+import fr.aboin.cockteirb.ui.ingredients.IngredientsFragment
+import fr.aboin.cockteirb.ui.search.SearchFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
+
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tab_layout)
+        tabLayout.addOnTabSelectedListener(this)
+
+        displaySearchFragment()
+    }
+
+    private fun displaySearchFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, SearchFragment.newInstance("hello", "world"))
+            .commit()
+    }
+
+    private fun displayCategoriesFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, CategoriesFragment.newInstance("hello", "world"))
+            .commit()
+    }
+
+    private fun displayIngredientsFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, IngredientsFragment.newInstance("hello", "world"))
+            .commit()
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        Log.i("MainActivity", "onTabReselected")
+        tab?.let {
+            when (it.position) {
+                0 -> displaySearchFragment()
+                1 -> displayCategoriesFragment()
+                2 -> displayIngredientsFragment()
+            }
+        }
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+        Log.i("MainActivity", "onTabUnselected")
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+        Log.i("MainActivity", "onTabReselected")
     }
 }
